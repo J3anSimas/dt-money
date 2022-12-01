ab0da07c2fbef9f9383fb9bacffdf18c939ee1b6
Jean Simas <jeansimas16@gmail.com>
2022-12-01 13:42:41 -0300

    Criando e estilizando component summary

--- src/pages/Transactions/transactions.page.tsx
+++ src/pages/Transactions/transactions.page.tsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header/header.component'
+import Summary from '../../components/Summary/summary.component'
 import { TransactionsContainer } from './transactions.styles'
 
 export default function Transactions (): JSX.Element {
   return (
     <TransactionsContainer>
       <Header />
+      <Summary />
     </TransactionsContainer>
   )
 }
