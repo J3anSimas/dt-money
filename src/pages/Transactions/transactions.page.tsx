e84a8c0d536c525d3aa28ad24858e16f271352ad
Jean Simas <jeansimas16@gmail.com>
2022-12-02 13:30:24 -0300

    styling search form and transactions table

--- src/pages/Transactions/transactions.page.tsx
+++ src/pages/Transactions/transactions.page.tsx
@@ -1,12 +1,44 @@
 import Header from '../../components/Header/header.component'
 import Summary from '../../components/Summary/summary.component'
-import { TransactionsContainer } from './transactions.styles'
+import SearchForm from './components/SearchForm/search-form.component'
+import { PriceHightlight, TransactionsContainer, TransactionsTable, TransactionsTableContainer } from './transactions.styles'
 
 export default function Transactions (): JSX.Element {
   return (
     <TransactionsContainer>
       <Header />
       <Summary />
+      <TransactionsTableContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento do site</td>
+              <td>
+                <PriceHightlight
+                variant='income'
+                >
+                  R$ 12.400,00
+                </PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento do site</td>
+              <td>
+                <PriceHightlight
+                variant='outcome'
+                >
+                  R$ 12.400,00
+                </PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsTableContainer>
     </TransactionsContainer>
   )
 }
