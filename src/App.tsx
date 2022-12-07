2763a615b23dd56cb0175743d914047e0f4e24a8
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-12-06 22:52:23 -0300

    Added Transactions Provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import TransactionsProvider from './contexts/transactions.context'
 import Transactions from './pages/Transactions/transactions.page'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
-export function App (): JSX.Element {
-  return (<ThemeProvider theme={defaultTheme}>
-    <Transactions />
-    <GlobalStyle />
-  </ThemeProvider>)
+export function App(): JSX.Element {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
