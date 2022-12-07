315cd633e0fe940818d9e863abbc0cf3c6580eda
Jean Simas <jeansimas16@gmail.com>
2022-12-07 14:15:54 -0300

    Implementing react hook form

--- src/components/NewTransactionModal/new-transaction-modal.component.tsx
+++ src/components/NewTransactionModal/new-transaction-modal.component.tsx
@@ -1,5 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { useForm, Controller } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import {
   CloseButton,
   Content,
@@ -8,7 +11,32 @@ import {
   TransactionTypeButton
 } from './new-transaction-modal.styles'
 
+const createTransactionFormValidationSchema = z.object({
+  description: z.string().min(1),
+  price: z.number().min(0),
+  category: z.string(),
+  type: z.enum(['income', 'outcome'])
+})
+
+type TNewTransactionFormInputs = z.infer<
+  typeof createTransactionFormValidationSchema
+>
+
 export default function NewTransactionModal(): JSX.Element {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<TNewTransactionFormInputs>({
+    resolver: zodResolver(createTransactionFormValidationSchema)
+  })
+
+  async function handleCreateTransaction(
+    data: TNewTransactionFormInputs
+  ): Promise<void> {
+    console.log(data)
+  }
   return (
     <Dialog.Portal>
       <Overlay />
@@ -17,21 +45,48 @@ export default function NewTransactionModal(): JSX.Element {
         <CloseButton>
           <X size={24} />
         </CloseButton>
-        <form action="">
-          <input type="text" placeholder="Descrição" required />
-          <input type="text" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
-          <TransactionType>
-            <TransactionTypeButton value="income" variant="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton value="outcome" variant="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
-          <button type="submit">Cadastrar</button>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form onSubmit={handleSubmit(handleCreateTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="text"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => (
+              <TransactionType
+                onValueChange={field.onChange}
+                value={field.value}
+              >
+                <TransactionTypeButton value="income" variant="income">
+                  <ArrowCircleUp size={24} />
+                  Entrada
+                </TransactionTypeButton>
+                <TransactionTypeButton value="outcome" variant="outcome">
+                  <ArrowCircleDown size={24} />
+                  Saída
+                </TransactionTypeButton>
+              </TransactionType>
+            )}
+          />
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/search-form.component.tsx
+++ src/pages/Transactions/components/SearchForm/search-form.component.tsx
@@ -1,10 +1,19 @@
 import { MagnifyingGlass } from 'phosphor-react'
+import { useState } from 'react'
 import { SearchFormContainer } from './search-form.styles'
 
-export default function SearchForm (): JSX.Element {
+export default function SearchForm(): JSX.Element {
+  const [filter, setFilter] = useState('')
+
+  function handleSearchTransition(): void {}
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder='Busque por transações' />
+    <SearchFormContainer onSubmit={handleSearchTransition}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
       <button type="submit">
         <MagnifyingGlass size={20} />
         Busca
